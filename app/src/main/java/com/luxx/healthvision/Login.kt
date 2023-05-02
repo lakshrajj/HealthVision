780d05cfa07d63ceb20b98e39d8e5d2ebde30bde
lakshrajj <lakshrajme@gmail.com>
2023-05-03 01:55:18 +0530

    a lot of things

---
+++ app/src/main/java/com/luxx/healthvision/Login.kt
@@ -0,0 +1,90 @@
+package com.luxx.healthvision
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+class Login : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        var signinBtn = findViewById<Button>(R.id.signinBtn)
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("532447449127-9obk4rtjoffqab2bq74lnsvi0a8kksop.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        signinBtn.setOnClickListener {
+            signInGoogle()
+        }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(GoogleSignIn.getLastSignedInAccount(this)!=null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+
+    }
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){  val account : GoogleSignInAccount? = task.result
+            if(account != null){
+                createNewAccount(account)
+            }
+        }
+    }
+
+    private fun createNewAccount(account: GoogleSignInAccount) {
+        val cred = GoogleAuthProvider.getCredential(account.idToken, null)
+
+
+        auth.signInWithCredential(cred).addOnCompleteListener {
+            if (it.isSuccessful){
+
+                Toast.makeText(this, "Sign-In Success", Toast.LENGTH_LONG).show()
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Sign-In Failed", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
